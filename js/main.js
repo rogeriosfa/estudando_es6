6dc1688c1693eaa2d1d9fcd8793b976054eb5a24
Rogério Rodrigues <rogeriosfa@gmail.com>
2018-01-22 22:26:52 -0200

    arrow function

--- js/main.js
+++ js/main.js
@@ -8,3 +8,37 @@ var Cliente = function Cliente(nome, email) {
     this.email = email;
     this.nome = nome;
 };
+
+//antigo
+// var bob = {
+//     _name: "bob",
+//     _frieds: ["Guilherme", "Camila", "João"],
+//     printFriends: function printFriends() {
+//         var self = this; 
+//         this._frieds.forEach(function (f) {
+//             return console.log(self._name + " knows " + f);
+//         });
+//     }
+// };
+
+// bob.printFriends();
+
+var color = function color() {
+    return console.log("red");
+};
+color();
+
+//novo
+var bob = {
+    _name: "bob",
+    _frieds: ["Guilherme", "Camila", "João"],
+    printFriends: function printFriends() {
+        var _this = this;
+
+        this._frieds.forEach(function (f) {
+            return console.log(_this._name + " knows " + f);
+        });
+    }
+};
+
+bob.printFriends();
--- src/app.js
+++ src/app.js
@@ -5,3 +5,32 @@ class Cliente {
     }
 }
 
+//antigo
+// var bob = {
+//     _name: "bob",
+//     _frieds: ["Guilherme", "Camila", "João"],
+//     printFriends: function printFriends() {
+//         var self = this; 
+//         this._frieds.forEach(function (f) {
+//             return console.log(self._name + " knows " + f);
+//         });
+//     }
+// };
+
+// bob.printFriends();
+
+var color = () => console.log("red");
+color();
+
+
+//novo
+var bob = {
+    _name: "bob",
+    _frieds: ["Guilherme", "Camila", "João"],
+    printFriends: function printFriends() {
+        this._frieds.forEach(f => console.log(this._name + " knows " + f)
+        );
+    }
+};
+
+bob.printFriends();
